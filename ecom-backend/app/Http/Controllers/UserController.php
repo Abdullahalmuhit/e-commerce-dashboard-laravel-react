cabceb698ec130a4eea3a9f81171c8b36d2f630a
Abdullahalmuhit <abdullahalmuhit1296@gmail.com>
2022-03-06 10:37:59 +0600

    added new file

---
+++ ecom-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+        return response()->json('User successfully created', $user);
+        
+    }
+}
