6ce8afd6bc04d0fedc9f7fa33fd214fcfc0e7aa2
Abdullahalmuhit <abdullahalmuhit1296@gmail.com>
2022-03-06 15:42:41 +0600

    Added send value

--- ecom-backend/app/Http/Controllers/UserController.php
+++ ecom-backend/app/Http/Controllers/UserController.php
@@ -18,4 +18,14 @@ class UserController extends Controller
         return response()->json('User successfully created', $user);
         
     }
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password))
+        {
+            return "Error User Not Found";
+        }
+        return $user;
+
+    }
 }
--- ecom-backend/routes/api.php
+++ ecom-backend/routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
+
